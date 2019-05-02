634e64bd8fcf5d255ca0247f591ed54cd91a9e45
sngshivang <sng.shivang@gmail.com>
2019-05-02 11:00:18 +0530

    new file:   Manage.js 	new file:   public/Mange.css 	new file:   public/back2.jpg 	new file:   views/Manage.ejs 	new file:   views/Userscutlist.ejs 	new file:   views/Userslist.ejs 	new file:   views/credit.ejs

---
+++ Manage.js
@@ -0,0 +1,112 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyparser = require("body-parser");
+mongoose.connect("mongodb://localhost/Mange",{useNewUrlParser:true});
+
+
+var http = require('http');
+var fs = require('fs');
+app = express();
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+var mangschema = new mongoose.Schema({
+	name : String,
+	Email : String,
+	Credit : Number,
+});
+
+var Man = mongoose.model("Man", mangschema);
+
+/*Man.create({name : "Rahul Rana", Email:"ranasingh@gamil.com", Credit:3500},
+	function(err, cam){
+		if(err){
+			console.log(err);
+		}
+		else
+		{
+			console.log("Success");
+		}
+	});*/
+
+app.get("/", function(req, res){
+	res.render("Manage");
+});
+
+
+app.get("/userlist", function(req,res){
+	Man.find({}, function(err,docs){
+		if(err)
+			res.json(err);
+		else
+			res.render("Userslist",{men:docs})
+	});
+});
+
+
+app.post("/transfer/:ID",function(req,res){
+
+var amout=req.body.Amount;
+var id=req.params.ID;
+console.log(id,amout);
+	Man.find({}, function(err,docs){
+		if(err)
+			res.json(err);
+		else
+			res.render("Userscutlist",{men:docs,amounttocut:amout,fromid:id})
+
+	});
+});
+
+
+app.get("/usercut/:id/:amount/:fromid",function(req,res){
+var id=req.params.id,
+	amount=req.params.amount,
+	fromid=req.params.fromid;
+console.log(fromid, id, amount);
+Man.findOneAndUpdate({ "_id": id },
+    { "$inc": { "Credit": amount } },{new:true},function(err, response) {
+ if (err) {
+ console.log("Error");
+} 
+});
+console.log("First")
+
+Man.findOneAndUpdate({ "_id": fromid },
+    { "$inc": { "Credit": -(amount) } },{new:true},function(err, response) {
+ if (err) {
+ console.log(err);
+} 
+});
+
+console.log("attempted")
+res.redirect("/userlist");
+});
+
+ app.get("/credit/:id", function(req, res){
+  var id=req.params.id;
+
+		Man.findById({"_id":id}, function(err,docs){
+		if(err)
+			res.json(err);
+		else
+			res.render("credit",{men:docs,ID:id})
+
+	});
+});
+
+/*app.post("/selectuser",function(req,res){
+
+var name=req.body.namedata;
+	res.redirect("/credit");
+})*/
+
+app.listen(9000, function(){
+     console.log("Server is listening");
+ });
+
+
+
+
+
